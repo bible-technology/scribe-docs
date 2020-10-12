7e9f2880d9649374e17d32bcc1b905e1d471f02e
Ashersam <ashersamuel06@gmail.com>
2020-10-12 12:51:09 +0530

    initial commit for docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/home/asherphilip/Documents/AG-docs/autographa-docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/asherphilip/Documents/AG-docs/autographa-docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/asherphilip/Documents/AG-docs/autographa-docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/asherphilip/Documents/AG-docs/autographa-docs/node_modules/infima/dist/css/default/default.css"),
+  require("/home/asherphilip/Documents/AG-docs/autographa-docs/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
+  require("/home/asherphilip/Documents/AG-docs/autographa-docs/src/css/custom.css"),
+];
