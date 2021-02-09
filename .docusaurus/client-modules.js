7af859b232a1338cf06f925c4236f395b7f6eefb
Ashersam <ashersamuel06@gmail.com>
2021-02-09 13:44:06 +0530

    delete .dcusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("C:\\Users\\asher\\Documents\\GitHub\\autographa-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\asher\\Documents\\GitHub\\autographa-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\asher\\Documents\\GitHub\\autographa-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\asher\\Documents\\GitHub\\autographa-docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\asher\\Documents\\GitHub\\autographa-docs\\node_modules\\@docusaurus\\theme-classic\\src\\prism-include-languages"),
-  require("C:\\Users\\asher\\Documents\\GitHub\\autographa-docs\\src\\css\\custom.css"),
-];
