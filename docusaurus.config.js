c5f105b07784d74aa6e3094b6f4028e0a155bced
Ashersam <ashersamuel06@gmail.com>
2021-04-05 12:47:05 +0530

    routing to docs page
    removed landing page and blog and some unwanted things

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,105 +1,74 @@
 module.exports = {
-  title: 'Autographa-docs',
-  tagline: 'Documentation for Autographa',
-  url: 'https://autographa-docs.netlify.app/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  favicon: 'img/logo.png',
-  organizationName: 'friendsofagape', // Usually your GitHub org/user name.
-  projectName: 'autographa-docs', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: 'Autographa',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/autographa_logo.svg',
-        srcDark: 'img/ag_logo.png',
-      },
-      items: [
-        {
-          to: 'docs',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/friendsofagape/autographa-docs',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/friendsofagape/autographa-docs',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/friendsofagape/autographa-docs/edit/main/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/friendsofagape/autographa-docs/edit/main/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
-    ],
-  ],
+	title: 'Autographa-docs',
+	tagline: 'Documentation for Autographa',
+	url: 'https://autographa-docs.netlify.app/',
+	baseUrl: '/',
+	onBrokenLinks: 'throw',
+	favicon: 'img/logo.png',
+	organizationName: 'friendsofagape', // Usually your GitHub org/user name.
+	projectName: 'autographa-docs', // Usually your repo name.
+	themeConfig: {
+		navbar: {
+			title: 'Autographa',
+			logo: {
+				alt: 'My Site Logo',
+				src: 'img/autographa_logo.svg',
+				srcDark: 'img/ag_logo.png',
+			},
+			items: [
+				{
+					to: 'docs',
+					activeBasePath: 'docs',
+					label: 'Docs',
+					position: 'left',
+				},
+				{
+					href: 'https://github.com/friendsofagape/autographa-docs',
+					label: 'GitHub',
+					position: 'right',
+				},
+			],
+		},
+		footer: {
+			style: 'dark',
+			links: [
+				{
+					title: 'Docs',
+					items: [
+						{
+							label: 'Style Guide',
+							to: 'docs/',
+						},
+						{
+							label: 'Second Doc',
+							to: 'docs/doc2/',
+						},
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} Autographa Project, Inc. Built with Docusaurus.`,
+		},
+	},
+	presets: [
+		[
+			'@docusaurus/preset-classic',
+			{
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					// Please change this to your repo.
+					editUrl:
+						'https://github.com/friendsofagape/autographa-docs/edit/main/',
+				},
+				blog: {
+					showReadingTime: true,
+					// Please change this to your repo.
+					editUrl:
+						'https://github.com/friendsofagape/autographa-docs/edit/main/',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			},
+		],
+	],
 };
