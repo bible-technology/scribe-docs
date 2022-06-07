2735d5648884eac390bc401eb7921361d8832730
samueljd <samuelj.ernest@gmail.com>
2022-06-07 17:28:31 +0530

    added versions page

---
+++ src/pages/versions.tsx
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Link from '@docusaurus/Link';
+import Translate from '@docusaurus/Translate';
+import {
+  useVersions,
+  useLatestVersion,
+} from '@docusaurus/plugin-content-docs/client';
+import Layout from '@theme/Layout';
+import Heading from '@theme/Heading';
+// import VersionsArchived from '@site/versionsArchived.json';
+
+// const VersionsArchivedList = Object.entries(VersionsArchived);
+
+function DocumentationLabel() {
+  return (
+    <Translate id="versionsPage.versionEntry.link">Documentation</Translate>
+  );
+}
+
+function ReleaseNotesLabel() {
+  return (
+    <Translate id="versionsPage.versionEntry.releaseNotes">
+      Release Notes
+    </Translate>
+  );
+}
+
+export default function Version(): JSX.Element {
+  const {
+    siteConfig: { organizationName, projectName },
+  } = useDocusaurusContext();
+  const versions = useVersions();
+  const latestVersion = useLatestVersion();
+  const currentVersion = versions.find(
+    (version) => version.name === 'current',
+  )!;
+  const pastVersions = versions.filter(
+    (version) => version !== latestVersion && version.name !== 'current',
+  );
+  const repoUrl = `https://github.com/${organizationName!}/${projectName!}`;
+
+  return (
+    <Layout
+      title="Versions"
+      description="Page listing all Autographa documentent versions">
+      <main className="container margin-vert--lg">
+        <Heading as="h1">
+          <Translate id="versionsPage.title">
+            Autographa documentation versions
+          </Translate>
+        </Heading>
+
+        <div className="margin-bottom--lg">
+          <Heading as="h3" id="next">
+            <Translate id="versionsPage.current.title">
+              Current version (Stable)
+            </Translate>
+          </Heading>
+          <p>
+            <Translate id="versionsPage.current.description">
+              Here you can find the documentation for current released version.
+            </Translate>
+          </p>
+          <table>
+            <tbody>
+              <tr>
+                <th>{latestVersion.label}</th>
+                <td>
+                  <Link to={latestVersion.path}>
+                    <DocumentationLabel />
+                  </Link>
+                </td>
+                <td>
+                  <a href={`${repoUrl}/releases/tag/v${latestVersion.name}`}>
+                    <ReleaseNotesLabel />
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        {currentVersion !== latestVersion && (
+          <div className="margin-bottom--lg">
+            <Heading as="h3" id="latest">
+              <Translate id="versionsPage.next.title">
+                Next version (Unreleased)
+              </Translate>
+            </Heading>
+            <p>
+              <Translate id="versionsPage.next.description">
+                Here you can find the documentation for work-in-process
+                unreleased version.
+              </Translate>
+            </p>
+            <table>
+              <tbody>
+                <tr>
+                  <th>{currentVersion.label}</th>
+                  <td>
+                    <Link to={currentVersion.path}>
+                      <DocumentationLabel />
+                    </Link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        )}
+      </main>
+    </Layout>
+  );
+}
