616a90c4b61657b2feff09baa61f0d271314d6da
samueljd <samuelj.ernest@gmail.com>
2022-06-20 18:42:30 +0530

    Readme file added

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# autographa-docs
+# Autographa Doc
+
 Documentation website for Autographa
+
+### Document Structure
+
+Every document has a unique id. By default, a document id is the name of the document (without the extension) relative to the root docs directory. However, the **last part** of the `id` can be defined by the user in the front matter. 
+
+```
+autographa-docs # Root directory
+└── docs
+    └── autographa
+        ├──about-this-guide
+        ├──getting-started
+          └── what-is-autographa.md
+        └── hello.md
+```
+
+### Creating a Doc Page
+
+Create a Markdown file, `new_doc_page.md`, and place it under the `docs` directory. If there is a category with multiple pages, create a directory and add all the pages to that. 
+
+At the top of the file, specify the unique id `id` , document title `title`  and title for the doc on the side bar `sidebar_label` in the front matter.
+
+```
+---
+id: new_page
+title: document title
+sidebar_label: label for the doc on the left pane.
+---
+
+Lorem ipsum
+
+
+```
+
+Add the page to the sidebars.js as below. The `id` and `sidebar_label` is used to create the structure of the site. 
+
+```
+module.exports = {
+  autographa: [
+   // Normal syntax
+    {
+      type: 'category',
+      label: 'categorylable',
+      items: [
+        '/new_page',
+      ],
+    },
+   // Short Hand syntax
+   { "category lable":[sidebar items]},
+  ],
+};
+```
+
+to add a page without a category set the type as doc and use the document id.
+
+```
+module.exports = {
+  autographa: [
+    {
+  	  type: 'doc',
+      label: 'sidebar label',
+      id: '/document_id',
+    }
+  ],
+};
+```
+
+
+
+### Adding assets
+
+Add images and other assets to a directory in the `static/autographaAssetsV2` and add them in the document in with <img> syntax with absolute paths. 
+
+```
+<img src="/autographaAssetsV2/path/image.png" width="100%" />
+```
+
+### 
+
--- sidebars.js
+++ sidebars.js
@@ -1,46 +1,61 @@
 module.exports = {
-  autographalive: {
-    'About this guide': [
-      'autographa-live/About-this-guide/p0-1-objective.md',
-      'autographa-live/About-this-guide/p0-2-how-use-guide.md',
-      'autographa-live/About-this-guide/p0-3-supported-sys.md',
-      'autographa-live/About-this-guide/p0-4-techncl-support.md',
-    ],
-    'Getting Started': [
-      'autographa-live/Getting-Started/p1-1-what-is-a.md',
-      'autographa-live/Getting-Started/p1-2-how-to-install.md',
-      'autographa-live/Getting-Started/p1-3-managing-autographa.md',
-    ],
-    "How To": [
-      "autographa-live/How-To/p2-1-overview.md",
-      "autographa-live/How-To/p2-2-sign-in.md",
-      {
-        "Project Listing": [
-          "autographa-live/How-To/p2-3-project-listing.md",
-          "autographa-live/How-To/Project-Listing/p2-3-1-starting-project.md",
-          "autographa-live/How-To/Project-Listing/p2-3-2-creating-project.md",
-          "autographa-live/How-To/Project-Listing/p2-3-3-advanced-settings.mdx.md",
-          "autographa-live/How-To/Project-Listing/p2-3-3-editing-project.md"
-        ],
-      },
-      {
-        "Text Translation Mode": [
-          "autographa-live/How-To/Text-Translate-Mode/p2-4-1-t-plane.md",
-          "autographa-live/How-To/Text-Translate-Mode/p2-4-2-add-columns.md",
-          "autographa-live/How-To/Text-Translate-Mode/p2-4-3-resource-types.md",
-          "autographa-live/How-To/Text-Translate-Mode/p2-4-4-import-resources.md",
-          "autographa-live/How-To/Text-Translate-Mode/p2-4-5-split-column.md",
-          "autographa-live/How-To/Text-Translate-Mode/p2-4-6-lock-unlock-editor.md",
-          "autographa-live/How-To/Text-Translate-Mode/p2-4-7-create-bookmarks.md",
-          "autographa-live/How-To/Text-Translate-Mode/p2-4-8-file-menu.mdx",
-          "autographa-live/How-To/Text-Translate-Mode/p2-4-9-edit.md",
-          "autographa-live/How-To/Text-Translate-Mode/p2-4-10-about.md",
-          "autographa-live/How-To/Text-Translate-Mode/p2-4-11-font.md",
-          "autographa-live/How-To/Text-Translate-Mode/p2-4-12-notifications.md",
-        ],
-      },
-      "autographa-live/How-To/p2-5-profile.md",
-    ],
-    'More Information': ['autographa-live/p3-more-info.md'],
-  },
-}
+  autographa: [
+    {
+      type: 'category',
+      label: 'About this guide',
+      items: [
+        'autographa/About-this-guide/p0-1-objective',
+        'autographa/About-this-guide/p0-2-how-use-guide',
+        'autographa/About-this-guide/p0-3-supported-sys',
+        'autographa/About-this-guide/p0-4-techncl-support',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Getting Started',
+      items: [
+        'autographa/Getting-Started/p1-1-what-is-a',
+        'autographa/Getting-Started/p1-2-how-to-install',
+        'autographa/Getting-Started/p1-3-managing-autographa',
+      ],
+    }, {
+      type: 'category',
+      label: 'How To',
+      items: [
+        "autographa/How-To/p2-1-overview",
+        "autographa/How-To/p2-2-sign-in",
+        {
+          "Project Listing": [
+            "autographa/How-To/p2-3-project-listing",
+            "autographa/How-To/Project-Listing/p2-3-1-starting-project",
+            "autographa/How-To/Project-Listing/p2-3-2-creating-project",
+            "autographa/How-To/Project-Listing/p2-3-3-advanced-settings",
+            "autographa/How-To/Project-Listing/p2-3-3-editing-project"
+          ],
+        },
+        {
+          "Text Translation Mode": [
+            "autographa/How-To/Text-Translate-Mode/p2-4-1-t-plane",
+            "autographa/How-To/Text-Translate-Mode/p2-4-2-add-columns",
+            "autographa/How-To/Text-Translate-Mode/p2-4-3-resource-types",
+            "autographa/How-To/Text-Translate-Mode/p2-4-4-import-resources",
+            "autographa/How-To/Text-Translate-Mode/p2-4-5-split-column",
+            "autographa/How-To/Text-Translate-Mode/p2-4-6-lock-unlock-editor",
+            "autographa/How-To/Text-Translate-Mode/p2-4-7-create-bookmarks",
+            "autographa/How-To/Text-Translate-Mode/p2-4-8-file-menu",
+            "autographa/How-To/Text-Translate-Mode/p2-4-9-edit",
+            "autographa/How-To/Text-Translate-Mode/p2-4-10-about",
+            "autographa/How-To/Text-Translate-Mode/p2-4-11-font",
+            "autographa/How-To/Text-Translate-Mode/p2-4-12-notifications",
+          ],
+        },
+        "autographa/How-To/p2-5-profile",
+      ],
+    },
+    {
+      type: 'doc',
+      label: 'More Information',
+      id: 'autographa/p3-more-info',
+    }
+  ],
+};
